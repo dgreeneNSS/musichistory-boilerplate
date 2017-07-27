15905b7f824ccc2bdbf10bf599aeb671dffb7199
Daniel Greene <d.b.greene101@gmail.com>
2017-07-27 10:46:06 -0500

    changes made and finished thanks Casey

--- javaSongs/songs.js
+++ javaSongs/songs.js
@@ -1,9 +1,29 @@
-/**
-+1. Add one song to the beginning and the end of the array.
-+2. Loop over the array, and remove any words or characters that obviously don't belong.
-+3. Find and replace the > character in each item with a - character.
-4. Add each string to the DOM in index.html in the main content area. 
-**/
+/*
++1. In the navigation bar, make sure you have two links labeled "List Music", and "Add Music".
++2. Add a DOM element that contains some input fields for the user to enter in the name of a song, the artist for the song, and the album. You do not need to enclose them in a <form> element because we're not actually submitting this form anywhere.
++3. Add a <button> element at the bottom of the input fields labeled "Add".
++ The input fields and the add button make up the Add Music View.
++ The existing view - the combination of the filter form and the song list - will be referred to as the List Music View.
++The Add Music View should not appear when the user first visits your page. The song list with the corresponding filter form should be visible.
++ When the user clicks on "Add Music" in the navigation bar, the List Music View should be hidden, and the Add Music View should be shown (see example wireframe).
++ When the user clicks on "List Music" in the navigation bar, the Add Music View should be hidden, and the List Music View should be shown (see example wireframe).
+Once the user fills out the song form and clicks the add button, you should collect all values from the input fields, add the song to your array of songs, and update the song list in the DOM.
+*/
+
+var listS = document.getElementById('songList');
+var hidden = document.getElementById('addMusicView');
+
+function HideSL() {
+        hidden.style.display = 'block';
+        listS.style.display = 'none';
+};
+
+function HideNM() {
+        hidden.style.display = 'none';
+        listS.style.display = 'block';
+};
+
+
 
 
 var songs = [];
@@ -32,6 +52,38 @@ for (i = 0; i < songs.length; i++){
         songs[i] = songs[i].replace('(', '')
     }
 }
+
+
+function AddSongs() {
+    console.log('hello');
+    var p = document.getElementById('songList');
+    songs.push(document.getElementById('songI').value + ' by ' + document.getElementById('artistsI').value + ' on the album ' + document.getElementById('albumI').value);
+    console.log('songs', songs);
+    songs.forEach(function(item){
+    document.getElementById("songList").innerHTML += `<li>${item}</li>`
+})
+
+};
+
+
 songs.forEach(function(item){
     document.getElementById("songList").innerHTML += `<li>${item}</li>`
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
